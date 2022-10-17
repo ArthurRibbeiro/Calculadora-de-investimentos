a63600123837a973627d9e9849cf8d6f50e0cb35
arthur <art.rib@hotmail.com>
2022-10-17 18:48:52 -0300

    Interface inicial

--- CFI/src/App.java
+++ CFI/src/App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String opcao;
+        Scanner scan = new Scanner(System.in);
+        do{
+            System.out.println("""
+            Selecione uma opção
+            1 - Incluir novo plano de contas
+            2 - Visualizar
+            3 - Excluir plano
+            0 - Sair""");
+            opcao = scan.nextLine();
+        
+        }while (!opcao.equals("0"));
     }
 }
