bc321710d3bd2b846032ffa2ae785c0c230167c9
arthur <art.rib@hotmail.com>
2022-10-18 08:18:13 -0300

    Criação das opções iniciais

--- CFI/src/App.java
+++ CFI/src/App.java
@@ -9,11 +9,44 @@ public class App {
             limpar();
             System.out.println("""
             Selecione uma opção
-            1 - Incluir novo plano de contas
-            2 - Visualizar
-            3 - Excluir plano
+            1 - Configurar salário
+            2 - Incluir novo plano de contas
+            3 - Editar plano de contas
+            4 - Visualizar
             0 - Sair""");
             opcao = scan.nextLine();
+            limpar(args);
+
+            switch (opcao){
+                case "1":
+                    System.out.println("Opção 1 em desenvolvimento");                    
+                    System.out.println("pressione enter para Continuar");
+                    scan.nextLine();
+                break;
+                case "2" :
+                    System.out.println("Opção 2 em desenvolvimento");
+                    System.out.println("pressione enter para Continuar");
+                    scan.nextLine();
+                break;
+                case "3":
+                    System.out.println("Opção 3 em desenvolvimento");
+                    System.out.println("pressione enter para Continuar");
+                    scan.nextLine();
+                break;
+                case "4":
+                    System.out.println("Opção 3 em desenvolvimento");
+                    System.out.println("pressione enter para Continuar");
+                    scan.nextLine();
+                break;
+                case "0":
+                System.out.println("Saindo...");
+                break;
+                default:
+                    System.out.println("Informe uma opção válida");
+                    System.out.println("pressione enter para Continuar");
+                    scan.nextLine();
+                break;
+            }
         
         }while (!opcao.equals("0"));
     }
